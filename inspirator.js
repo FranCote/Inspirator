c108c6107ff4f0df56a3a66ab93ed17f6acd83ff
Fran <75789621+FranCote@users.noreply.github.com>
2021-01-28 14:20:13 +0000

    first commit - data added

---
+++ inspirator.js
@@ -0,0 +1,13 @@
+let inspirator = {
+    strategies: [],
+    quotes: [],
+    jokes: [],
+}
+
+inspirator.strategies = ['State the problem in words as clearly as possible', 'What are you really thinking about just now? Incorporate', 'Be less critical more often', 'Go to an extreme, move back to a more comfortable place', 'Once the search is in progress, something will be found', 'Would anybody want it?'];
+
+inspirator.quotes = ['Simplicity is the ultimate sophistication. – Leonardo da Vinci', 'If I’m gonna tell a real story, I’m gonna start with my name. – Kendrick Lamar', 'Oh, the things you can find, if you don’t stay behind. – Dr. Seuss'
+];
+
+inspirator.jokes = ['Crime in multi-storey car parks. That is wrong on so many different levels. - Tim Vine (2011)', 'I have downloaded this new app. Its great, it tells you what to wear, what to eat and if you’ve put on weight. Its called the Daily Mail. - Hayley Ellis (2016)', 'When I was younger I felt like a man trapped inside a woman’s body. Then I was born. - Yianni (2015)'
+];
